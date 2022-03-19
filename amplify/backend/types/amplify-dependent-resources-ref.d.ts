f7a9c3ff4411bf5e9fa216fb7eda24390669791e
atsushi-ito-1 <atsushi.ito@jp.ricoh.com>
2022-03-19 17:46:33 +0900

    .

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,20 @@
+export type AmplifyDependentResourcesAttributes = {
+    "api": {
+        "amplifyauthtest1": {
+            "GraphQLAPIKeyOutput": "string",
+            "GraphQLAPIIdOutput": "string",
+            "GraphQLAPIEndpointOutput": "string"
+        }
+    },
+    "auth": {
+        "amplifyauthtest1e313d009": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string"
+        }
+    }
+}
--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,26 @@
+<script setup>
+import { Authenticator } from '@aws-amplify/ui-vue'
+import '@aws-amplify/ui-vue/styles.css'
+</script>
+
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <authenticator>
+    <template v-slot="{ user, signOut }">
+      <h1>Hello {{ user.username }}!</h1>
+      <button @click="signOut">Sign Out</button>
+      <div id="app">
+        <TodoTest />
+      </div>
+    </template>
+  </authenticator>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import TodoTest from "./components/TodoTest.vue"
 export default {
-  name: 'App',
+  name: 'app',
   components: {
-    HelloWorld
+    TodoTest
   }
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
